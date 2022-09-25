633396c3d2afc747c6bd651e93576b94bd8789b7
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2022-09-25 15:14:24 +0200

    add social and tools icons

--- config/index.js
+++ config/index.js
@@ -4,3 +4,47 @@ export const SECTIONS = [
   {name: 'Gallery', slug: 'gallery'},
   {name: 'Blog', slug: 'blog'}
 ]
+export const BRANDS = {
+  SOCIAL: [
+    {
+      name: 'facebook',
+      color: '#1877f2'
+    },
+    {
+      name: 'instagram',
+      color: '#ff0069'
+    },
+    {
+      name: 'whatsapp',
+      color: '#25d366'
+    },
+    {
+      name: 'twitter',
+      color: '#1d9bf0'
+    },
+    {
+      name: 'vero',
+      color: '#00cccc'
+    },
+    {
+      name: 'youtube',
+      color: '#ff0000'
+    },
+    {
+      name: 'linkedin',
+      color: '#0082be'
+    }
+  ],
+  TOOLS: [
+    {
+      name: 'nextjs'
+    },
+    {
+      name: 'vercel'
+    },
+    {
+      name: 'tailwindcss',
+      color: '#38bdf8'
+    }
+  ]
+}
