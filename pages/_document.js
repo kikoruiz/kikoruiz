557424d6fcf975bce0e2ad5344b81f7c3fec2a32
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2022-10-10 10:26:45 +0200

    add some small fixes

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,11 @@ export default function Document({locale}) {
         <meta name="theme-color" content="#171717" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
--- pages/sobre-mi.js
+++ pages/sobre-mi.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import useTranslation from 'next-translate/useTranslation'
+import {getPlaiceholder} from 'plaiceholder'
 import {fromLocalesToAlternates} from '../lib/mappers.js'
 import {getDescription} from '../lib/about-me.js'
 import {themeScreens} from '../lib/utils.js'
@@ -19,7 +20,7 @@ export default function AboutMe({avatar, description, alternates}) {
 
       <section className="flex flex-col p-6 sm:flex-row sm:p-0">
         <div className="w-full px-24 sm:mr-6 sm:w-1/3 sm:px-0 md:px-6 xl:px-12 2xl:px-24">
-          <div className="relative mb-6 aspect-square rounded-full border-8 border-neutral-600/30 sm:mb-0">
+          <div className="relative mb-6 aspect-square overflow-hidden rounded-full border-8 border-neutral-600/30 sm:mb-0">
             <Image
               src={avatar.src}
               layout="fill"
@@ -27,6 +28,8 @@ export default function AboutMe({avatar, description, alternates}) {
               objectFit="cover"
               alt={t('sections.about-me.name')}
               className="rounded-full"
+              placeholder="blur"
+              blurDataURL={avatar.base64}
               priority
             />
           </div>
@@ -47,6 +50,8 @@ export async function getStaticProps({locales, locale, defaultLocale}) {
     src: '/avatar.jpg',
     sizes: `(min-width: ${sm}) 33vw, 100vw`
   }
+  const {base64} = await getPlaiceholder(avatar.src)
+  avatar.base64 = base64
   const section = 'about-me'
   const alternates = await Promise.all(
     locales.map(await fromLocalesToAlternates({defaultLocale, section}))
