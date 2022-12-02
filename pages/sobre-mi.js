80e73b4f9193af9f22f929cd2cb7df319cc40478
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2022-12-02 14:59:07 +0100

    migrate to new images

--- pages/sobre-mi.js
+++ pages/sobre-mi.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from "next/legacy/image";
+import Image from "next/image";
 import {useRouter} from 'next/router'
 import useTranslation from 'next-translate/useTranslation'
 import {getPlaiceholder} from 'plaiceholder'
@@ -13,107 +13,106 @@ export default function AboutMe({avatar, description, alternates}) {
   const {locale} = useRouter()
   const {t} = useTranslation()
 
-  return (
-    <>
-      <Head>
-        <title>{`Kiko Ruiz / ${t('sections.about-me.name')}`}</title>
-        {alternates.map(({locale, href}) => (
-          <link key={locale} rel="alternate" hrefLang={locale} href={href} />
-        ))}
-      </Head>
+  return <>
+    <Head>
+      <title>{`Kiko Ruiz / ${t('sections.about-me.name')}`}</title>
+      {alternates.map(({locale, href}) => (
+        <link key={locale} rel="alternate" hrefLang={locale} href={href} />
+      ))}
+    </Head>
 
-      <section className="flex flex-col p-6 sm:flex-row sm:py-0">
-        <div className="mb-12 w-full sm:mr-6 sm:w-1/3">
-          <div className="relative mx-12 mb-6 aspect-square overflow-hidden rounded-full border-8 border-neutral-600/30 sm:mx-4 sm:mb-6 md:mx-6 xl:mx-14">
-            <Image
-              src={avatar.src}
-              layout="fill"
-              sizes={avatar.sizes}
-              objectFit="cover"
-              alt={t('sections.about-me.name')}
-              className="rounded-full"
-              placeholder="blur"
-              blurDataURL={avatar.base64}
-              priority
-            />
-          </div>
-          <dl className="rounded-lg bg-gradient-to-tl from-neutral-800/60 px-6 sm:px-3">
-            {Object.keys(PERSONAL_INFO).map(key => {
-              const value = PERSONAL_INFO[key]
-
-              function description() {
-                switch (key) {
-                  case 'phone':
-                    return (
-                      <a
-                        href={`tel:${value}`}
-                        className="hover:text-orange-200"
-                        title={value}
-                      >
-                        {value}
-                      </a>
-                    )
-                  case 'email':
-                    return (
-                      <a
-                        href={`mailto:${value}`}
-                        className="hover:text-orange-200"
-                        title={value}
-                      >
-                        {value}
-                      </a>
-                    )
-                  case 'location':
-                    return (
-                      <a
-                        href={`https://www.google.es/maps/place/${encodeURIComponent(
-                          value
-                        )}`}
-                        className="hover:text-orange-200"
-                        title={value}
-                        target="_blank"
-                        rel="noopener noreferrer nofollow"
-                      >
-                        {value}
-                      </a>
-                    )
-                  case 'birthday':
-                    const date = new Date(value)
-                    const birthday = new Intl.DateTimeFormat(locale, {
-                      day: '2-digit',
-                      month: '2-digit',
-                      year: 'numeric'
-                    }).format(date)
-
-                    return birthday
-                  default:
-                    return value
-                }
-              }
-
-              return (
-                <div
-                  className={`${personalInfoBackground(
-                    key
-                  )} relative border-b border-neutral-800 bg-[length:1.2rem] bg-[left_0.65rem_center] bg-no-repeat py-6 pl-14 before:absolute before:left-0 before:top-1/2 before:-z-10 before:-mt-5 before:h-10 before:w-10 before:rounded-xl before:bg-gradient-to-bl before:from-neutral-700 last:border-0`}
-                  key={key}
-                >
-                  <dt className="text-xs opacity-30">
-                    {t(`about-me:personal-info.${key}`)}
-                  </dt>
-                  <dd className="overflow-hidden text-ellipsis font-semibold">
-                    {description()}
-                  </dd>
-                </div>
-              )
-            })}
-          </dl>
+    <section className="flex flex-col p-6 sm:flex-row sm:py-0">
+      <div className="mb-12 w-full sm:mr-6 sm:w-1/3">
+        <div className="relative mx-12 mb-6 aspect-square overflow-hidden rounded-full border-8 border-neutral-600/30 sm:mx-4 sm:mb-6 md:mx-6 xl:mx-14">
+          <Image
+            src={avatar.src}
+            alt={t('sections.about-me.name')}
+            className="rounded-full"
+            placeholder="blur"
+            blurDataURL={avatar.base64}
+            priority
+            fill
+            sizes={avatar.sizes}
+            style={{
+              objectFit: "cover"
+            }} />
         </div>
+        <dl className="rounded-lg bg-gradient-to-tl from-neutral-800/60 px-6 sm:px-3">
+          {Object.keys(PERSONAL_INFO).map(key => {
+            const value = PERSONAL_INFO[key]
 
-        <Article content={description.content} className="flex-1" />
-      </section>
-    </>
-  )
+            function description() {
+              switch (key) {
+                case 'phone':
+                  return (
+                    <a
+                      href={`tel:${value}`}
+                      className="hover:text-orange-200"
+                      title={value}
+                    >
+                      {value}
+                    </a>
+                  )
+                case 'email':
+                  return (
+                    <a
+                      href={`mailto:${value}`}
+                      className="hover:text-orange-200"
+                      title={value}
+                    >
+                      {value}
+                    </a>
+                  )
+                case 'location':
+                  return (
+                    <a
+                      href={`https://www.google.es/maps/place/${encodeURIComponent(
+                        value
+                      )}`}
+                      className="hover:text-orange-200"
+                      title={value}
+                      target="_blank"
+                      rel="noopener noreferrer nofollow"
+                    >
+                      {value}
+                    </a>
+                  )
+                case 'birthday':
+                  const date = new Date(value)
+                  const birthday = new Intl.DateTimeFormat(locale, {
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: 'numeric'
+                  }).format(date)
+
+                  return birthday
+                default:
+                  return value
+              }
+            }
+
+            return (
+              <div
+                className={`${personalInfoBackground(
+                  key
+                )} relative border-b border-neutral-800 bg-[length:1.2rem] bg-[left_0.65rem_center] bg-no-repeat py-6 pl-14 before:absolute before:left-0 before:top-1/2 before:-z-10 before:-mt-5 before:h-10 before:w-10 before:rounded-xl before:bg-gradient-to-bl before:from-neutral-700 last:border-0`}
+                key={key}
+              >
+                <dt className="text-xs opacity-30">
+                  {t(`about-me:personal-info.${key}`)}
+                </dt>
+                <dd className="overflow-hidden text-ellipsis font-semibold">
+                  {description()}
+                </dd>
+              </div>
+            )
+          })}
+        </dl>
+      </div>
+
+      <Article content={description.content} className="flex-1" />
+    </section>
+  </>;
 }
 
 export async function getStaticProps({locales, locale, defaultLocale}) {
