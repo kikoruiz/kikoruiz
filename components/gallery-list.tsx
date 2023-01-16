5820a708c537b8b0f416704ef8b1ea8742f14b15
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-01-16 17:18:16 +0100

    add more gap large in gallery list for xl screens

--- components/gallery-list.tsx
+++ components/gallery-list.tsx
@@ -68,7 +68,7 @@ export default function GalleryList({
         </div>
       )}
 
-      <div className="columns-1 gap-3 space-y-3 pb-3 sm:columns-2 lg:columns-3">
+      <div className="columns-1 gap-3 space-y-3 pb-3 sm:columns-2 lg:columns-3 xl:gap-4 xl:space-y-4 xl:pb-4">
         {items.map(
           (
             {
@@ -93,7 +93,7 @@ export default function GalleryList({
               (isSecondImage && items[0].image.orientation === 'horizontal')
             const {src, css} = image
             const className = `group inline-flex flex-col-reverse break-inside-avoid-column w-full after:absolute after:inset-0 after:h-full after:w-full after:border after:border-transparent hover:after:border-orange-300${
-              isFirstImage ? ' mt-3' : ''
+              isFirstImage ? ' mt-3 xl:mt-4' : ''
             }`
             const captionBaseClassName =
               'relative bg-gradient-to-r from-neutral-900 text-xs lg:text-sm'
