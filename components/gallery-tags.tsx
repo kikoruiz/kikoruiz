09dda3529162b5948e97b3e7f3e97308c590faef
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-02-15 21:20:10 +0100

    fix some stuff

--- components/gallery-tags.tsx
+++ components/gallery-tags.tsx
@@ -9,9 +9,9 @@ export default function GalleryTags({tags}: GalleryTagsProps) {
           key={id}
           href={href}
           title={name}
-          className="mt-3 block break-inside-avoid-column break-words rounded border border-neutral-800 bg-gradient-to-b from-neutral-800/60 to-neutral-800/30 p-3 font-extrabold text-neutral-300/60 first-of-type:mt-0 hover:border-orange-300/60 hover:to-transparent hover:text-orange-300/60"
+          className="mt-3 flex break-inside-avoid-column break-words rounded border border-neutral-800 bg-gradient-to-b from-neutral-800/60 to-neutral-800/30 p-3 font-extrabold text-neutral-300/60 first-of-type:mt-0 hover:border-orange-300/60 hover:to-transparent hover:text-orange-300/60"
         >
-          <span className="font-thin"># </span>
+          <span className="mr-1 font-thin">#</span>
           {name}
         </Link>
       ))}
