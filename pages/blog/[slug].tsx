d92c5730c203cc1cdcd717fad8eef262174afdf3
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-05-30 18:26:16 +0200

    fix text alignment

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -39,9 +39,9 @@ export default function Post({post, alternates}: PostProps) {
       </Head>
 
       <article className="mx-auto p-6 xl:max-w-5xl">
-        <header className="pt-9 sm:text-center sm:pt-0">
+        <header className="pt-9 text-center sm:pt-0">
           {!BLOG.AVAILABLE_LOCALES.includes(locale) && (
-            <div className="mb-12 w-full text-sm font-extralight leading-normal drop-shadow-lg">
+            <div className="mb-12 w-full text-sm font-extralight text-left sm:text-center leading-normal drop-shadow-lg">
               <p className="flex items-center justify-center gap-1.5 rounded bg-orange-800 px-6 py-3 drop-shadow">
                 <IconInformationCircle className="w-6" />
                 {t('common:blog.post.available-locales.warning')}
