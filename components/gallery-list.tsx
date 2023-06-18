4e55418bbb56f0feeabf689490ecbe1ec887458f
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-06-18 23:01:52 +0200

    fix anchor urls

--- components/gallery-list.tsx
+++ components/gallery-list.tsx
@@ -76,14 +76,18 @@ export default function GalleryList({
         <div className="mx-auto mb-12 flex max-w-xs flex-wrap justify-center gap-3 font-extralight sm:max-w-none">
           {subcategories.map(({id, emoji}) => {
             const name = t(`gallery.albums.${category}.subcategories.${id}`)
+            const href = `/${getSlug(t('sections.gallery.name'))}/${getSlug(
+              t(`gallery.albums.${category}.name`)
+            )}#${getSlug(name)}`
             const Icon = icons[`Icon${getCapitalizedName(id)}`]
 
             return (
               <Link
                 key={id}
-                href={`#${getSlug(name)}`}
+                href={href}
                 title={name}
                 scroll={false}
+                shallow
                 className={`rounded-lg bg-gradient-to-t from-neutral-600/20 to-neutral-600/10 px-3 py-2 text-neutral-300/60 hover:bg-neutral-600/10 hover:text-orange-300 ${
                   emoji ? 'inline-block' : 'flex items-center'
                 }`}
