85dc85c7d2fcc3275e620dd5657f285d48acfdbc
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-06-19 13:38:39 +0200

    add transition colors to picture card title

--- components/picture-card.tsx
+++ components/picture-card.tsx
@@ -38,7 +38,7 @@ export default function PictureCard({
     >
       <figcaption className={captionClassName}>
         <header
-          className={`drop-shadow group-hover:text-orange-300 ${
+          className={`drop-shadow transition-colors group-hover:text-orange-300 ${
             isAlbum
               ? 'break-words text-6xl font-thin leading-none text-neutral-300/90 sm:text-5xl md:text-6xl'
               : 'text-sm font-bold'
