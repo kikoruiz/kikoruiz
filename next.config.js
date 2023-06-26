6843f2d4aa24aa6aaa06810e7a4bd1e6be5b47f1
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-06-26 10:31:27 +0200

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,47 +1,52 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
 const nextTranslate = require('next-translate-plugin')
 const {withPlaiceholder} = require('@plaiceholder/next')
 
-module.exports = withPlaiceholder(
-  nextTranslate({
-    reactStrictMode: true,
-    images: {
-      formats: ['image/avif', 'image/webp']
-    },
-    i18n: {
-      locales: ['es', 'ca', 'en'],
-      defaultLocale: 'es'
-    },
-    async rewrites() {
-      return [
-        {
-          source: '/en/about-me',
-          destination: '/en/sobre-mi',
-          locale: false
-        },
-        {
-          source: '/en/gallery',
-          destination: '/en/galeria',
-          locale: false
-        },
-        {
-          source: '/en/gallery/:slug',
-          destination: '/en/galeria/:slug',
-          locale: false
-        },
-        {
-          source: '/en/gallery/tags/:tag',
-          destination: '/en/galeria/tags/:tag',
-          locale: false
-        }
-      ]
-    },
-    webpack: config => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack']
-      })
+module.exports = withBundleAnalyzer(
+  withPlaiceholder(
+    nextTranslate({
+      reactStrictMode: true,
+      images: {
+        formats: ['image/avif', 'image/webp']
+      },
+      i18n: {
+        locales: ['es', 'ca', 'en'],
+        defaultLocale: 'es'
+      },
+      async rewrites() {
+        return [
+          {
+            source: '/en/about-me',
+            destination: '/en/sobre-mi',
+            locale: false
+          },
+          {
+            source: '/en/gallery',
+            destination: '/en/galeria',
+            locale: false
+          },
+          {
+            source: '/en/gallery/:slug',
+            destination: '/en/galeria/:slug',
+            locale: false
+          },
+          {
+            source: '/en/gallery/tags/:tag',
+            destination: '/en/galeria/tags/:tag',
+            locale: false
+          }
+        ]
+      },
+      webpack: config => {
+        config.module.rules.push({
+          test: /\.svg$/,
+          use: ['@svgr/webpack']
+        })
 
-      return config
-    }
-  })
+        return config
+      }
+    })
+  )
 )
