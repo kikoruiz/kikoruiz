7695d0fa325d2e2e78a07d354ec78355b59bfd78
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2024-01-18 00:35:36 +0100

    improve print card

--- components/print-card.tsx
+++ components/print-card.tsx
@@ -24,7 +24,7 @@ export default function PrintCard({
       key={id}
       className="group p-3 bg-neutral-800 hover:bg-neutral-700 rounded-md break-inside-avoid-column"
     >
-      <div className="relative bg-gradient-to-bl from-neutral-600 via-neutral-200 to-neutral-400 p-[10%] drop-shadow-md group-hover:from-neutral-100 group-hover:to-neutral-100 group-hover:drop-shadow-xl before:absolute before:z-10 before:content-[''] before:top-0 before:right-0 before:border-solid before:border-b-[.75em] before:border-r-[.75em] before:border-y-neutral-300/60 before:border-x-neutral-800 before:transition-[border-width] group-hover:before:border-y-neutral-300/90 group-hover:before:border-x-neutral-700 group-hover:before:border-b-[1.5em] group-hover:before:border-r-[1.5em]">
+      <div className="relative bg-gradient-to-bl from-neutral-600 via-neutral-200 to-neutral-400 p-[10%] drop-shadow-md group-hover:from-neutral-100 group-hover:to-neutral-100 group-hover:drop-shadow-xl before:absolute before:z-10 before:content-[''] before:top-0 before:right-0 before:border-solid before:border-b-[.75em] before:border-r-[.75em] before:border-y-neutral-300/60 before:border-x-neutral-800 before:transition-[border-width] before:duration-300 group-hover:before:border-y-neutral-300/90 group-hover:before:border-x-neutral-700 group-hover:before:border-b-[1.5em] group-hover:before:border-r-[1.5em]">
         <Image
           src={src}
           url={url}
@@ -42,15 +42,20 @@ export default function PrintCard({
         )}
       </div>
 
-      <div className="flex items-center justify-between mt-3 py-1.5 pl-1.5">
-        <span className="font-black text-2xl group-hover:text-neutral-100 drop-shadow">
-          {t('price', {count: price})}
-        </span>
+      <div className="flex items-start justify-between mt-3 py-1.5 pl-1.5">
+        <div className="flex flex-col gap-1.5">
+          <header className="font-thin">{name}</header>
+
+          <span className="font-black text-2xl group-hover:text-neutral-100 drop-shadow">
+            {t('price', {count: price})}
+          </span>
+        </div>
 
         <Button
           intent="accent"
           isRounded
           className="snipcart-add-item"
+          title={t('add-to-cart')}
           data-item-id={id}
           data-item-name={name}
           data-item-price={price}
