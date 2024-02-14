8b0cfd2ba8e7549041f48b08ebebdb1b8956f2a5
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2024-02-14 16:26:04 +0100

    add noindex on non-desired hosts

--- next.config.mjs
+++ next.config.mjs
@@ -60,6 +60,25 @@ const nextConfig = withBundleAnalyzer({
           }
         ]
       },
+      async headers() {
+        return [
+          {
+            source: '/:path*',
+            has: [
+              {
+                type: 'host',
+                value: 'kikoruiz.vercel.app'
+              }
+            ],
+            headers: [
+              {
+                key: 'X-Robots-Tag',
+                value: 'noindex'
+              }
+            ]
+          }
+        ]
+      },
       webpack: config => {
         config.module.rules.push({
           test: /\.svg$/,
