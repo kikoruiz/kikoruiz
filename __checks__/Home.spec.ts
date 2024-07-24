9acb76e4998cf6c45bbcabd56aa1c6b38b98338f
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2024-07-24 11:37:19 +0200

    change some tests

--- __checks__/Home.spec.ts
+++ __checks__/Home.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test'
 
 test('Home Page', async ({page}) => {
-  const targetUrl = process.env.ENVIRONMENT_URL || 'https://kikoruiz.vercel.app'
+  const targetUrl = process.env.ORIGIN || 'https://kikoruiz.vercel.app'
   const response = await page.goto(targetUrl)
 
   expect(response?.status()).toBeLessThan(400)
