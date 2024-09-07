5af0d2f96359e9bb4960470934df9535cfb2291b
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2024-09-08 00:53:22 +0200

    fix simple pages

--- components/simple-page.tsx
+++ components/simple-page.tsx
@@ -4,7 +4,7 @@ import Logo from 'assets/brand/logo.svg'
 export default function SimplePage({title}: {title: string}) {
   return (
     <HomeBlock className="relative flex items-center justify-center px-12 mt-9 sm:mt-3">
-      <h1 className="absolute left-0 p-6 text-center font-black drop-shadow-lg text-3xl sm:text-4xl">
+      <h1 className="absolute left-0 w-full p-6 text-center font-black drop-shadow-lg text-3xl sm:text-4xl">
         {title}
       </h1>
 
