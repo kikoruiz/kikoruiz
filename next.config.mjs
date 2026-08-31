ff2950c09f567174252751aabd3e7a1fc584cb7c
Kiko Ruiz <kikoruiz@icloud.com>
2026-08-31 17:17:06 +0200

    update deps

--- next.config.mjs
+++ next.config.mjs
@@ -27,6 +27,14 @@ const nextConfig = withBundleAnalyzer({
       async headers() {
         return http.headers
       },
+      turbopack: {
+        rules: {
+          '*.svg': {
+            loaders: ['@svgr/webpack'],
+            as: '*.js'
+          }
+        }
+      },
       webpack: config => {
         config.module.rules.push({
           test: /\.svg$/,
